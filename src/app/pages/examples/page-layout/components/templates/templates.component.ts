ad3ab21f39b3f9537f54f698b361eefb81f51805
oussema kassis <oussema.kassis@softtodo.com>
2023-05-26 14:29:23 +0100

    [TASK] LAYOUT ROUTING

--- src/app/pages/examples/page-layout/components/templates/templates.component.ts
+++ src/app/pages/examples/page-layout/components/templates/templates.component.ts
@@ -13,16 +13,17 @@ export class TemplatesComponent implements OnInit {
     name: string;
     preview: string;
     duration: string;
+    category: string;
   }[] = [
-    {id:0,name:"",preview:"/assets/video/video1_m.mp4", duration:""},
-    {id:1,name:"",preview:"/assets/video/video2_m.mp4", duration:""},
-    {id:2,name:"",preview:"/assets/video/video3_m.mp4", duration:""},
-    {id:3,name:"",preview:"/assets/video/video4_m.mp4", duration:""},
-    {id:4,name:"",preview:"/assets/video/video5_m.mp4", duration:""},
-    {id:5,name:"",preview:"/assets/video/video6_m.mp4", duration:""},
-    {id:6,name:"",preview:"/assets/video/video7_m.mp4", duration:""},
-    {id:7,name:"",preview:"/assets/video/video8_m.mp4", duration:""},
-    {id:8,name:"",preview:"/assets/video/video9_m.mp4", duration:""}
+    {id:0, name:"test", duration:"00:00:10", preview:"/assets/video/video1_m.mp4", category:"category"},
+    {id:1,name:"name",duration:"00:00:10",preview:"/assets/video/video2_m.mp4",category:"category"},
+    {id:2,name:"random",duration:"00:00:10",preview:"/assets/video/video3_m.mp4",category:"category"},
+    {id:3,name:"whatever",duration:"00:00:10",preview:"/assets/video/video4_m.mp4",category:"category"},
+    {id:4,name:"",duration:"00:00:10",preview:"/assets/video/video5_m.mp4",category:"category"},
+    {id:5,name:"",duration:"00:00:10",preview:"/assets/video/video6_m.mp4",category:"category"},
+    {id:6,name:"",duration:"00:00:10",preview:"/assets/video/video7_m.mp4",category:"category"},
+    {id:7,name:"",duration:"00:00:10",preview:"/assets/video/video8_m.mp4",category:"category"},
+    {id:8,name:"",duration:"00:00:10",preview:"/assets/video/video9_m.mp4",category:"category"}
   ];
 
   constructor(private router: Router) { }
