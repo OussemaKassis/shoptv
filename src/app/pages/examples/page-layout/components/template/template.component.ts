0892d551c42416276a8b1a85a7159acf169d8b05
OussemaKassis <oussemakassis97@gmail.com>
2023-06-04 09:59:51 +0100

    [TASK] logo + register page

--- src/app/pages/examples/page-layout/components/template/template.component.ts
+++ src/app/pages/examples/page-layout/components/template/template.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  list: {
+    id:number;
+    name: string;
+    type: string;
+  }[] = [{id:0,name:"headline text",type:"text"},{id:1,name:"product image",type:"image"}];
+
   constructor() { }
 
   ngOnInit(): void {
